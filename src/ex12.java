3e468410e1735a91f0c2fff000840c7bda314b79
Lucas Silveira <lucassilveira07.ls@gmail.com>
2023-06-02 20:25:52 -0300

    exercício

--- src/ex12.java
+++ src/ex12.java
@@ -1,4 +1,7 @@
-/* um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”. */
+/* um programa que leia um valor qualquer e apresente uma mensagem dizendo 
+em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este 
+valor se encontra. Obviamente se o valor não estiver em nenhum destes 
+intervalos, deverá ser impressa a mensagem “Fora de intervalo”. */
 
 
 
