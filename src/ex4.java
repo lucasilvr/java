1f4bc1385679a36baad039c38b70bd504fe5a8ed
Lucas Silveira <lucassilveira07.ls@gmail.com>
2023-06-01 21:15:53 -0300

    exercicio

---
+++ src/ex4.java
@@ -0,0 +1,28 @@
+//Um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex4 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		int id_func;
+		double num_htrab, valor_h, salario;
+		
+		id_func = sc.nextInt();
+		num_htrab = sc.nextDouble();
+		valor_h = sc.nextDouble();
+		
+		salario = num_htrab * valor_h;
+		
+		System.out.println("O numero do funcionario é: " + id_func);
+		System.out.printf("O salario do funcionario é: RS$%.2f", salario);
+		
+		sc.close();
+
+	}
+
+}
---
+++ src/ex5.java
@@ -0,0 +1,33 @@
+//Um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ex5 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		int cod_peca1, cod_peca2, qnt_peca1, qnt_peca2;
+		double valor_peca1, valor_peca2, pagar;
+		
+		cod_peca1 = sc.nextInt();
+		qnt_peca1 = sc.nextInt();
+		valor_peca1 = sc.nextDouble();
+		
+		cod_peca2 = sc.nextInt();
+		qnt_peca2 = sc.nextInt();
+		valor_peca2 = sc.nextDouble();
+		
+		pagar = (valor_peca1 * qnt_peca1) + (valor_peca2 * qnt_peca2);
+		
+		System.out.printf("Total a pagar: R$ %.2f", pagar);
+		
+		sc.close();
+		
+		
+
+	}
+
+}
